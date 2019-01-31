5f5cb8dd901a2c26e71aa20ca93c3f31b3153b81
bttk <bttk@google.com>
2019-01-31 17:37:47 +0000

    Add a script to regenerate docs
    Update io_bazel_rules_sass to 1.16.1
    Update io_bazel_skydoc to 0.2.0
    Add a run_regen_docs.sh for use in Travis CI

---
+++ scripts/run_regen_docs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Copyright 2018 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eu
+
+# -------------------------------------------------------------------------------------------------
+# Asked to do a docs build.
+if [[ -n "${REGEN_DOCS:-}" ]]; then
+  if ! diff -u <(git status --porcelain=v2) /dev/null; then
+    echo >&2 "Repository is not in a clean state"
+    exit 1
+  fi
+
+  ./docs/regen.sh
+
+  if ! diff -u <(git status --porcelain=v2) /dev/null; then
+    echo >&2 "The documentation is out of date. Run ./docs/regen.sh"
+  fi
+fi
